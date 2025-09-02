61c1427b2eb1009f425a7f0f36a71a250ac2f704
Skyler <shall088@fiu.edu>
2025-09-02 10:12:29 -0400

    Added Recyclepedia content

--- src/app/_components/Projects.tsx
+++ src/app/_components/Projects.tsx
@@ -3,17 +3,20 @@ const Projects = () => {
     {
       title: "Spotify Data Visualizer",
       description: "Created and led a project to teach the fundamentals of HTML, CSS, and JavaScript using Chart.js to convert a music dataset into interactive charts.",
-      link: "#"
+      link: "#",
+      image: "/project-1.svg"
     },
     {
       title: "Recyclepedia",
       description: "Led a team in collaboration with Miami-Dade County to redevelop their Recyclepedia website, resulting in 200 new active monthly users. The project used React, Firebase, Tailwind, and Next.js.",
-      link: "#"
+      link: "https://recyclepediamdc.com/en",
+      image: "/RecyclepediaSS.png"
     },
     {
       title: "DriveWise",
       description: "Led a team to develop a route-planning tool that notifies users of commute times based on desired thresholds. The project integrated the Google Directions API and the Twilio API.",
-      link: "#"
+      link: "#",
+      image: "/project-3.svg"
     }
   ];
 
@@ -24,13 +27,13 @@ const Projects = () => {
         <div className="project-list">
           {projects.map((project, index) => (
             <div key={index} className="project-item">
-              <img src={`/project-${index + 1}.svg`} alt={project.title} className="project-thumb" />
+              <img src={project.image} alt={project.title} className="project-thumb" />
               <div className="project-info">
                 <h3 className="section-heading">{project.title}</h3>
                 <p className="section-body">{project.description}</p>
                 <div>
                   <a href={project.link} className="text-neonBlue font-semibold hover:underline">
-                    Learn More &rarr;
+                    Check it out! &rarr;
                   </a>
                 </div>
               </div>
